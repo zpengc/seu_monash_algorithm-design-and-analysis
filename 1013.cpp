96868dbcc242a8f8afd8b7395a0b7ec92170ed0d
zpengc <zhupengcheng@gmail.com>
2021-10-23 14:12:22 +0800

    upload 1013

---
+++ 1013.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+int merge(int arr[], int temp[], int left, int mid,
+          int right)
+{
+    int i, j, k;
+    int inv_count = 0;
+ 
+    i = left; /* i is index for left subarray*/
+    j = mid; /* j is index for right subarray*/
+    k = left; /* k is index for resultant merged subarray*/
+    while ((i <= mid - 1) && (j <= right)) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        }
+        else {
+            temp[k++] = arr[j++];
+ 
+            inv_count = inv_count + (mid - i);
+        }
+    }
+ 
+    while (i <= mid - 1)
+        temp[k++] = arr[i++];
+ 
+    while (j <= right)
+        temp[k++] = arr[j++];
+ 
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+ 
+    return inv_count;
+}
+
+int _mergeSort(int arr[], int temp[], int left, int right)
+{
+    int inv_count = 0;
+    if (right > left) {
+        int mid = (right + left) / 2;
+ 
+        inv_count += _mergeSort(arr, temp, left, mid);
+        inv_count += _mergeSort(arr, temp, mid + 1, right);
+ 
+        inv_count += merge(arr, temp, left, mid + 1, right);
+    }
+    return inv_count;
+}
+
+int mergeSort(int arr[], int array_size)
+{
+    int temp[array_size];
+    return _mergeSort(arr, temp, 0, array_size - 1);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int size;
+        cin>>size;
+        int arr[size];
+        for(int j=0;j<size;j++)
+        {
+            cin>>arr[j];
+        }
+        cout<<mergeSort(arr, size)<<endl;
+    }
+    return 0;
+}
